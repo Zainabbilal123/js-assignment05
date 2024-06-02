138157a95104496bad9aebe1c49a644f68ff3970
Your Name <bilalzainab123agmail.com>
2024-06-02 11:29:32 -0700

    added files

---
+++ script.js
@@ -0,0 +1,30 @@
+// var badalhai = false
+//   if (badalhai ) {
+//     console.log("barish hogi")
+//   }
+//   else {
+//     console.log("barish nai hogi")
+//   }
+
+// var brtcard = +prompt("balance")
+//      if (brtcard >= 50){
+//         console.log("you can go")
+//      }
+//      else {
+//         console.log("you can not enter")
+//      }
+
+// var burger = +prompt("ur balance")
+//  if (burger >= 300){
+//     console.log("u can get ur burger")
+//  }
+//  else {
+//     console.log("u can not get ur burger")
+//  }
+// var number = +prompt("odd or even")
+//       if (number 2% == 0 ) {
+//         console.log("equaly divide by 2")
+//       }
+//       else {
+//         console.log("not equally divided by 2")
+//       }
