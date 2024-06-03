ca89aca93086883df34f2115417fce2115ee6685
Your Name <bilalzainab123agmail.com>
2024-06-03 01:21:53 -0700

    did changes

--- script.js
+++ script.js
@@ -28,3 +28,36 @@
 //       else {
 //         console.log("not equally divided by 2")
 //       }
+
+// var orange = 100
+// var mango = 400
+// var totalbill = orange + mango 
+// console.log(totalbill)
+
+// var traficlight = prompt("enter signal")
+//   if (traficlight == "green") {
+//     console.log("go")
+//   } else if (traficlight == "yellow") {
+//     console.log("get ready")
+//   } else{
+//     console.log("stop")
+//   }
+
+// var shoppingamount = +prompt("ur parches")
+//   if (shoppingamount >= 500) {
+//     console.log("you can get 20% disscount")
+//   } else if (shoppingamount >= 100) {
+//      console.log("you can get  10% disscount ")
+//   } else {
+//     console.log("you can get 5% disscount")
+//   }
+
+
+// var atm = +prompt("ur pocket")
+// if (atm > 5000){
+
+// console.log("you can get your money")}
+//       else {
+
+
+// console.log("you can not get your money")}
